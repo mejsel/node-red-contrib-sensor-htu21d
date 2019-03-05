bfa8b55f9993288c88116841837b195511488dd3
Marcel Indlekofer <marcel.indlekofer@gmail.com>
2019-03-05 17:09:30 +0100

    testing

--- sensor-htu21d.js
+++ sensor-htu21d.js
@@ -1,12 +1,33 @@
+let Htu21d = require('sensor-htu21d');
+
 module.exports = function(RED) {
+
 
   function Htu21dNode(config) {
 
     RED.nodes.createNode(this, config);
     var node = this;
+    
+
+    //let htu = new Htu21d(1, 1000);
+
+    /*
+    htu.on('fertig', (data) => {
+      msg.payload = data;
+      node.send(msg);
+    });
+    */
+
+    //htu.start();
+
+    var timer = setInterval( () => { 
+      var msg = {payload: "htu timer!"};
+      node.send(msg);
+    }, 1000);
 
     node.on('input', function(msg) {
       msg.payload = { temperature: 1.23, humidity: 4.56 };
+      node.send(msg);
     });
 
   }
